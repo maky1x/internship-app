632ec8733c2992c5d238818a53b036e9d5ef4218
maky1x <neskovicmaki@gmail.com>
2024-10-30 19:21:46 +0100

    Added a connection to the local MySQL database along with the parameters.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 use axum::{response::IntoResponse, routing::get, Json, Router};
 use tokio::net::TcpListener;
+use std::sync::Arc;
+use dotenv::dotenv;
+use sqlx::mysql::{MySqlPool, MySqlPoolOptions};
+
+pub struct AppState {
+    db: MySqlPool,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api/test-route", get(test_route_check));
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    let pool = match MySqlPoolOptions::new()
+    .max_connections(10)
+    .connect(&database_url)
+    .await
+    {
+        Ok(pool)=> {
+            println!("Connection to database is successful");
+            pool
+        }
+        Err(err)=> {
+            println!("Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let app = Router::new().route("/api/test-route", get(test_route_check))
+    .with_state(Arc::new(AppState { db:pool.clone()}));
 
     println!("Server started successfully at 0.0.0.0:8080");
 
