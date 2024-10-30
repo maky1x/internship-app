bd748e8bd9232b82de13b3fd6d3861ef864eae47
maky1x <neskovicmaki@gmail.com>
2024-10-30 18:17:03 +0100

    Added the needed dependencies along with the code for starting the server on a specific port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::IntoResponse, routing::get, Json, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/test-route", get(test_route_check));
+
+    println!("Server started successfully at 0.0.0.0:8080");
+
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app.into_make_service())
+    .await
+    .unwrap();
+}
+
+pub async fn test_route_check() -> impl IntoResponse {
+    const MESSAGE: &str = "API service";
+
+    let json_response = serde_json::json!({
+        "status": "ok",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
 }
